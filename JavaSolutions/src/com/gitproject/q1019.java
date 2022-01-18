72b7d58f9dc179eaedae4c542e5d6dcc86c9a616
hello-sabira <sabira.khan248@gmail.com>
2022-01-18 10:47:22 +0300

    solution

---
+++ JavaSolutions/src/com/gitproject/q1019.java
@@ -0,0 +1,18 @@
+package com.gitproject;
+
+import java.util.Scanner;
+
+public class q1019 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int time = input.nextInt();
+        int hour = time / 3600;
+        int minute = (time % 3600)/60;
+        int second = ((time%3600)%60)%60;
+
+
+        System.out.printf("%d:%d:%d\n", hour, minute,second);
+        
+    }
+
+}
