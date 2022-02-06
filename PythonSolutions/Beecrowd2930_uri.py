5497bbc9c4a0e088a660ad174f9e9da99198d240
hello-sabira <sabira.khan248@gmail.com>
2022-02-06 18:26:00 +0300

    solutions

---
+++ PythonSolutions/Beecrowd2930_uri.py
@@ -0,0 +1,11 @@
+e, d = input().split(" ")
+e = int(e)
+d = int(d)
+if d - e >= 3:
+    print("Muito bem! Apresenta antes do Natal!")
+elif d - e < 0:
+    print("Eu odeio a professora!")
+elif d - e < 3:
+    print("Parece o trabalho do meu filho!")
+    d += 2
+    print("TCC Apresentado!") if (d <= 24) else print("Fail! Entao eh nataaaaal!")
