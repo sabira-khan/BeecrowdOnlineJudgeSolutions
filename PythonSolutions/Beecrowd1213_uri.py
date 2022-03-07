fe2e975a9cd470450fc5f7e79c97c6ba158ed387
hello-sabira <sabira.khan248@gmail.com>
2022-03-07 11:24:27 +0300

    solution

---
+++ PythonSolutions/Beecrowd1213_uri.py
@@ -0,0 +1,16 @@
+while True:
+    try:
+        n = int(input())
+        p = c = 1
+        while p % n != 0:
+            p = (10 * p + 1) % n
+            c += 1
+        print(c)
+
+    except EOFError:
+        break
+
+"""
+p += 10 * c
+c *= 10 works but will give TLE so be careful
+"""
